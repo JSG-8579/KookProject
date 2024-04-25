a0098525c4f0d48dfc6f34165e7dbdc6d76c2361
JSG-8579 <rkfntmdrhks@naver.com>
2024-04-25 17:54:02 +0900

    231

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 import GoogleLogin from "@/components/service/GoogleLogin";
 import NaverLogin from "@/components/service/NaverLogin";
 import Link from "next/link";
 import {Nerko_One} from "next/font/google"; 
 import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
+// import { useSession } from 'next-auth/react';
 
 const nerko = Nerko_One({
     preload: false,
@@ -16,11 +16,11 @@ const nerko = Nerko_One({
 export default function Home() {
 
   const router:any = useRouter();
-  const { data: session, status }: any = useSession();
+  // const { data: session, status }: any = useSession();
 
-    if (session) {
-      router.push('/home');
-    }
+    // if (session) {
+    //   router.push('/home');
+    // }
 
   return (
     <>
